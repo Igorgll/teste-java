28935eb32ee0410af684ce558adaffcd2730bf60
igorgll <igorlimagn@gmail.com>
2023-01-12 08:12:57 -0300

    @igorgll [Initializing project and populating h2 Database table]

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE tb_clientes (
+    id IDENTITY NOT NULL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    dataNascimento VARCHAR(255) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
+    logradouro VARCHAR(255) NOT NULL,
+    cep VARCHAR(255) NOT NULL,
+    numero VARCHAR(255) NOT NULL,
+    cidade VARCHAR(255) NOT NULL
+);
+
+
+-- POPULANDO A TABELA DE INICIO
+INSERT INTO tb_clientes(nome, dataNascimento, endereco, logradouro, cep, numero, cidade) 
+VALUES('João Silva', '01/01/2000', 'Rua dos Pinheiros', 'Praça da Sé', '01000-000', '100', 'São Paulo');
+
+INSERT INTO tb_clientes(nome, dataNascimento, endereco, logradouro, cep, numero, cidade) 
+VALUES('Maria Garcia', '05/03/1995', 'Avenida Paulista', 'Rua Augusta', '04567-890', '25', 'São Paulo');
+
+INSERT INTO tb_clientes(nome, dataNascimento, endereco, logradouro, cep, numero, cidade) 
+VALUES('Carlos Martinez', '15/07/1985', 'Rua das Flores', 'Avenida Beira-Mar', '12000-345', '50', 'Florianópolis');
+
+INSERT INTO tb_clientes(nome, dataNascimento, endereco, logradouro, cep, numero, cidade) 
+VALUES('Ana Rodriguez', '20/09/1990', 'Rua das Palmeiras', 'Rua do Ouvidor', '20000-123', '75', 'Salvador');
+
+INSERT INTO tb_clientes(nome, dataNascimento, endereco, logradouro, cep, numero, cidade) 
+VALUES('Pedro Gomes', '30/12/1980', 'Rua dos Pássaros', 'Praça da Liberdade', '30000-456', '120', 'Belo Horizonte');
